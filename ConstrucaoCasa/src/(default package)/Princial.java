317687dba5e61817f7f7b84f0ac185d6a66c0538
mateushbernardes <mateushenriquebernardes@gmail.com>
2024-12-03 23:37:20 -0300

    atividade incompleta

---
+++ ConstrucaoCasa/src/(default package)/Princial.java
@@ -0,0 +1,11 @@
+//package (default package);
+
+//import controle.Controladora;
+
+public class Princial {
+    public static void main(String[] args) {
+        
+        Controladora controladora = new Controladora();
+        controladora.exibeMenu();
+    }
+}
